67e990c673f8139e46e82510ed2e1c6771ad2949
Luis Vega <luiscvega@gmail.com>
2014-02-19 10:47:36 +0800

    Initial commit

---
+++ test/outflow.js
@@ -0,0 +1,34 @@
+var logick = require("../lib/outflow");
+var assert = require("assert");
+
+var createPerson = outflow({
+  attributes: ["first_name", "last_name"],
+
+  validations: function (assert, attributes) {
+    assert(attributes.first_name.length > 0, ["first name", "not present"]);
+    assert(attributes.last_name.length > 0, ["last name", "not present"]);
+  },
+
+  success: function (attributes) {
+    return attributes;
+  }
+});
+
+it("should run", function () {
+  createPerson({
+    first_name: "John",
+    last_name: "Doe"
+  }, function (err, result) {
+    assert(Object.keys(err).length === 0);
+    assert.deepEqual(result, { first_name: "John", last_name: "Doe" });
+  });
+});
+
+it("should validate", function () {
+  createPerson({
+    first_name: "",
+    last_name: ""
+  }, function (err, result) {
+    assert.deepEqual(err, { "first name": ["not present"], "last name": ["not present"] });
+  });
+});
